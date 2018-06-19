ba53b103c5fb5450aa3e3176de93bc34dbc5c65b
marlonchiu <knight1898@outlook.com>
2018-06-19 14:42:32 +0800

    chap04 jsonp recmommenddata slider

--- docs/chap04_推荐页面开发.md
+++ docs/chap04_推荐页面开发.md
@@ -77,7 +77,15 @@ import {commonParams, options} from './config'
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
-
+ /*
+  * Object.assign() 方法用于将所有可枚举属性的值从一个或多个源对象复制到目标对象。
+  *       将返回目标对象。
+  *   语法 Object.assign(target, ...sources)
+  *       target 目标对象
+  *       sources 源对象
+  *    如果目标对象中的属性具有相同的键，则属性将被源中的属性覆盖。
+  *    后来的源的属性将类似地覆盖早先的属性。
+  * */
   const data = Object.assign({}, commonParams, {
     platform: 'h5',
     uin: 0,
--- src/api/recommend.js
+++ src/api/recommend.js
@@ -5,7 +5,30 @@ import {commonParams, options} from './config'
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
-
+  /*
+  * Object.assign() 方法用于将所有可枚举属性的值从一个或多个源对象复制到目标对象。
+  *       将返回目标对象。
+  *   语法 Object.assign(target, ...sources)
+  *       target 目标对象
+  *       sources 源对象
+  *    如果目标对象中的属性具有相同的键，则属性将被源中的属性覆盖。
+  *    后来的源的属性将类似地覆盖早先的属性。
+  *
+  *    目标对象https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?
+  *    g_tk=1928093487&inCharset=utf-8&outCharset=utf-8&notice=0&format=jsonp&platform=h5&uin=0&needNewCode=1
+  *    &jsonpCallback=__jp0
+  *
+  *    data = {
+  *     g_tk: 1928093487,
+  *     inCharset: 'utf-8',
+  *     outCharset: 'utf-8',
+  *     notice: 0,
+  *     format: 'jsonp'
+  *     platform: 'h5',
+  *     uin: 0,
+  *     needNewCode: 1
+  *    }
+  * */
   const data = Object.assign({}, commonParams, {
     platform: 'h5',
     uin: 0,
