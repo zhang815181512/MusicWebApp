bb910b4d6d5b27758ab62c59849d154faca08690
marlonchiu <knight1898@outlook.com>
2018-11-10 19:01:43 +0800

    vuex player state

---
+++ src/common/js/config.js
@@ -0,0 +1,14 @@
+/*
+ *  Created by : ZhaoJiandong
+ *  Modified time: 2018/11/10 18:08
+ */
+
+/*
+ * 项目相关的配置项，统一管理
+ * */
+
+export const playMode = {
+  sequence: 0,
+  loop: 1,
+  random: 2
+}
--- src/store/state.js
+++ src/store/state.js
@@ -2,8 +2,18 @@
  *  Created by : ZhaoJiandong
  *  Modified time: 2018/11/3 11:57
  */
+
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  // 全局控制播放器的状态
+  playing: false,  // 播放状态
+  fullScreen: false,
+  playlist: [],
+  sequenceList: [],  // 顺序播放列表
+  mode: playMode.sequence,   // 播放模式
+  currentIndex: -1  // 当前播放
 }
 
 export default state
