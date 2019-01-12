1c8c962255c96b5a607b5d311cc413a0b1977c60
MarlonChiu <knight1898@outlook.com>
2019-01-12 15:01:49 +0800

    rank toplist data

---
+++ src/api/rank.js
@@ -0,0 +1,14 @@
+// recommend.js 封装网络请求方法
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getTopList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+  return jsonp(url, data, options)
+}
